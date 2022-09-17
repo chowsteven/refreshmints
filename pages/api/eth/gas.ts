759c729763195b182b5a562a6bb8349ccdd86a33
sc <stevencstuy@gmail.com>
2022-09-17 19:16:14 -0400

    Add comments

--- pages/api/eth/gas.ts
+++ pages/api/eth/gas.ts
@@ -4,13 +4,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // fetch gas prices
   const url = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETHERSCAN_API_KEY}`;
   const response = await fetch(url);
 
+  // if error, show error message
   if (!response.ok) {
     res.status(404).json({ message: 'Unexpected error occurred.' });
   }
 
+  // send gas prices as json
   const gas = await response.json();
   res.status(200).json(gas);
 }
--- pages/api/eth/price.ts
+++ pages/api/eth/price.ts
@@ -4,13 +4,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // fetch eth price
   const url = `https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API_KEY}`;
   const response = await fetch(url);
 
+  // if error, show error message
   if (!response.ok) {
     res.status(404).json({ message: 'Unexpected error occurred.' });
   }
 
+  // send eth price as json
   const price = await response.json();
   res.status(200).json(price);
 }
