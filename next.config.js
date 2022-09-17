fd8246b90ddc86191abb429150afdfc20043ed10
sc <stevencstuy@gmail.com>
2022-09-17 19:15:19 -0400

    Add image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['api.reservoir.tools'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
