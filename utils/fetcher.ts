1377aad1a1f48a8c7c8b26fd6887f066fa609fd9
sc <stevencstuy@gmail.com>
2022-09-22 22:43:00 -0400

    Change error branch conditional

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -2,7 +2,8 @@ export const fetcher = async (url: string) => {
   const res = await fetch(url);
   const data = await res.json();
 
-  if (res.status !== 200) {
+  // TODO: improve error handling
+  if (!res.ok) {
     throw new Error(data.message);
   }
   return data;
