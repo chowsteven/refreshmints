4e648dfa9f58d76c3c8a14ad5feb039332c7e9ae
sc <stevencstuy@gmail.com>
2022-09-22 22:41:41 -0400

    Replace live feed page with firehose

---
+++ pages/firehose.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Firehose: NextPage = () => {
+  return (
+    <div className='flex justify-center pt-36 text-4xl font-semibold'>
+      This page is currently undergoing maintenance and waiting on API updates.
+    </div>
+  );
+};
+
+export default Firehose;
--- pages/live-feed.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-
-const LiveFeed: NextPage = () => {
-  return <div></div>;
-};
-
-export default LiveFeed;
