99012bc83172260f579d4bf4dd3bf39a85da2e09
sc <stevencstuy@gmail.com>
2022-09-22 22:31:52 -0400

    Add collection listings and sales interfaces

---
+++ interfaces/CollectionListingsData.ts
@@ -0,0 +1,67 @@
+export interface SingleListing {
+  id: string;
+  kind: string;
+  side: string;
+  status: string;
+  tokenSetId: string;
+  tokenSetSchemaHash: string;
+  contract: string;
+  maker: string;
+  taker: string;
+  price: {
+    currency: {
+      contract: string;
+      name: string;
+      symbol: string;
+      decimal: number;
+    };
+    amount: {
+      raw: string;
+      decimal: number;
+      usd: number;
+      native: number;
+    };
+    netAmount: {
+      raw: string;
+      decimal: number;
+      usd: number;
+      native: number;
+    };
+  };
+  validFrom: number;
+  validUntil: number;
+  quantityFilled: number;
+  quantityRemaining: number;
+  metadata: {
+    kind: string;
+    data: {
+      collectionName: string;
+      tokenName: string;
+      image: string;
+    };
+  };
+  source: {
+    id: string;
+    name: string;
+    icon: string;
+    url: string;
+  };
+  feeBps: number;
+  feeBreakdown: [
+    {
+      bps: number;
+      kind: string;
+      recipient: string;
+    }
+  ];
+  expiration: number;
+  isReservoir: boolean | null;
+  isDynamic: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CollectionListingsData {
+  orders: SingleListing[];
+  continuation: string;
+}
---
+++ interfaces/CollectionSalesData.ts
@@ -0,0 +1,47 @@
+export interface SingleSale {
+  id: string;
+  saleId: string;
+  token: {
+    contract: string;
+    tokenId: string;
+    name: string | null;
+    image: string | null;
+    collection: {
+      id: string;
+      name: string;
+    };
+  };
+  orderId: string;
+  orderSource: string;
+  orderSide: string;
+  orderKind: string;
+  from: string;
+  to: string;
+  amount: string;
+  fillSource: string;
+  block: number;
+  txHash: string;
+  logIndex: number;
+  batchIndex: number;
+  timestamp: number;
+  price: {
+    currency: {
+      contract: string;
+      name: string;
+      symbol: string;
+      decimals: number;
+    };
+    amount: {
+      raw: BigInt;
+      decimal: number;
+      usd: number;
+      native: number;
+    };
+  };
+  washTradingScore: number;
+}
+
+export interface CollectionSalesData {
+  sales: SingleSale[];
+  continuation: string;
+}
