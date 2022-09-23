1a7b0fb5caf213919f108b7925dc806109b96e58
sc <stevencstuy@gmail.com>
2022-09-23 00:43:07 -0400

    Add maintenance info on firehose and trending pages

--- pages/firehose.tsx
+++ pages/firehose.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Firehose: NextPage = () => {
   return (
-    <div className='flex justify-center pt-36 text-4xl font-semibold'>
-      This page is currently undergoing maintenance and waiting on API updates.
-    </div>
+    <>
+      <Head>
+        <title>Firehose</title>
+        <link rel='icon' href='/images/favicon.ico' />
+      </Head>
+
+      <div className='flex justify-center pt-36 text-4xl font-semibold'>
+        This page is currently undergoing maintenance and waiting on API
+        updates.
+      </div>
+    </>
   );
 };
 
--- pages/trending.tsx
+++ pages/trending.tsx
@@ -2,7 +2,19 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Trending: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>Trending</title>
+        <link rel='icon' href='/images/favicon.ico' />
+      </Head>
+
+      <div className='flex justify-center pt-36 text-4xl font-semibold'>
+        This page is currently undergoing maintenance and waiting on API
+        updates.
+      </div>
+    </>
+  );
 };
 
 export default Trending;
